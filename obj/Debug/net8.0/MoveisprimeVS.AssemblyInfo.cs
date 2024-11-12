4c6fc4ab9d6846d6a1b44e2b6067fa1189713e02
Ruan2701006 <ruan.polovina@aluno.senai.br>
2024-11-12 17:41:43 -0300

    1211

--- obj/Debug/net8.0/MoveisprimeVS.AssemblyInfo.cs
+++ obj/Debug/net8.0/MoveisprimeVS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoveisprimeVS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902157cdb4957a74b64055defcb680fcfec555b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80c3ebe9fed3dfb5e2c71ea62d6fc904f91756b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoveisprimeVS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoveisprimeVS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
